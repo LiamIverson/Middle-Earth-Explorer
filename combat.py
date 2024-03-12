b921e795cc7e97afe62f29babcbf240146448f49
Graison Mitio <graison.mitio@gmail.com>
2024-03-11 21:31:03 -0600

    Added class template for combat to start flushing out attributes

---
+++ combat.py
@@ -0,0 +1,11 @@
+class Combat:
+    def __init__(self):
+        self.npc_fighters = []
+        self.player_fighter = []
+        self.npc_health = []
+        self.player_health = 0
+        self.npc_weapons = []
+        self.player_weapons = []
+    def fight():
+        pass
+    
